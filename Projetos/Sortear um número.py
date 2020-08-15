edbde56e0cc577caef1da28283b8e1eb225be947
Nuno Fernandes <nunoagf@live.com.pt>
2020-08-15 14:36:14 +0100

    Atualização do código

--- Projetos/Sortear um número.py
+++ Projetos/Sortear um número.py
@@ -1,13 +1,23 @@
 import random
 from time import sleep
 
-print("=" * 25)
-print("Sortear um número inteiro")
-print("=" * 25)
-minimo = int(input("Qual é o valor mínimo a sortear? "))
-maximo = int(input("Qual é o valor máximo a sortear? "))
+def sorteio():
+    print("=" * 25)
+    print("Sortear um número inteiro")
+    print("=" * 25)
+    minimo = int(input("Qual é o valor mínimo a sortear? "))
+    maximo = int(input("Qual é o valor máximo a sortear? "))
 
-escolha = random.randint(minimo, maximo)
-sleep(1)
+    escolha = random.randint(minimo, maximo)
+    sleep(1)
 
-print("O número sorteado foi {}.".format(escolha))
+    print("O número sorteado foi {}.".format(escolha))
+sorteio()
+continuar = str(input("Deseja voltar a sortear? (S/N)")).capitalize()
+while continuar == "S":
+    sorteio()
+    continuar = str(input("Deseja voltar a sortear? (S/N)")).capitalize()
+if continuar == "N":
+    print("Sorteio terminado.")
+else:
+    print("Opção inválida.")
