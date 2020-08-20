53312e3ea46e62dba7265327a5b8a1fde0a1137a
Nuno Fernandes <nunoagf@live.com.pt>
2020-08-20 14:49:45 +0100

    Atualização do código

--- Projetos/Mad Lips (english).py
+++ Projetos/Mad Lips (english).py
@@ -1,3 +1,4 @@
+import time
 def mad_lips():
 
     adjective = str(input("Tell me an adjective:"))
@@ -16,7 +17,7 @@ def mad_lips():
     food2 = str(input("Tell me a food:"))
     number2 = str(input("Tell me a number:"))
 
-
+    time.sleep(2)
     print("Pizza was invented by a {} {} chef named {}.".format(adjective, nationality, person_name))
     print("To make a pizza, you need to take a limp of {}, and make a thin, round {} {}.".format(noun, adjective2, noun2))
     print("Then you cover it with {} sauce, {} cheese, and fresh chopped {}.".format(adjective3, adjective4, plurarl_noun))
@@ -26,11 +27,15 @@ def mad_lips():
     print("If I could, I would eat pizza {} times a day!".format(number2))
 
 mad_lips()
+time.sleep(1)
 cont = str(input("Do you want to do it again? (Y/N) ")).capitalize()
 while cont == "Y":
+    time.sleep(1)
     mad_lips()
     cont = str(input("Do you want to do it again? (Y/N) ")).capitalize()
 if cont == "N":
+    time.sleep(1)
     print("Mad lips finish.")
 else:
+    time.sleep(1)
     print("Option invalid.")
